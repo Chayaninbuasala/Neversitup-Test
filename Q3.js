3e32c1460551e544aa92ec20b9ac56c6ba1c8091
Chayanin Buasala <chayanin.gamer@gmail.com>
2023-11-23 22:57:33 +0700

    test: 💍 fix code

--- Q3.js
+++ Q3.js
@@ -1,7 +1,19 @@
 function isOod(array) {
-    return array + " should return"+array.filter((num) => num % 2 == 0 )+", because it occurs "+array.filter((num) => num % 2 == 0 ).length +" times (which is odd)."
+    let isOods = array.filter((num) => num % 2 == 0 )
+    if (isOods.length > 0 && isOods.some((num) => num !== 0)) {
+        return array + " should return " + numberToArray(Math.max(...isOods)).filter((num) => num % 2 === 0) + ", because it occurs " + numberToArray(Math.max(...isOods)).filter((num) => num % 2 === 0).length + " times (which is odd).";
+      } else {
+        return array + " should return " + array.filter((num) => num % 2 === 0) + ", because it occurs " + array.filter((num) => num % 2 === 0).length + " times (which is odd).";
+      }
+      
 }
 
+function numberToArray(number) {
+    // Convert number to string and split it into an array of characters
+    const digitsArray = String(number).split('').map(Number);
+
+    return digitsArray;
+}
 console.log(isOod([7]));
 console.log(isOod([0]));
 console.log(isOod([1,1,2]));
